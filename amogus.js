b64c87a0e807d59df685cd8a2b00ee2649708776
RandomHtmlGuy <124398893+fitnessgrampacertestJS@users.noreply.github.com>
2023-04-03 09:25:34 -0600

    Update amogus.js

--- amogus.js
+++ amogus.js
@@ -5,9 +5,60 @@ var intervalID;
 function cow() {
 var x=0, y=0;
       return Math.random() < 0.5;
-      var win = window.open('image2.html', "",
+ var win = window.open('image2.html', "",
       "width=" + w + ",height=" + h);
       x = (screen.width/2)-(w/2);
+      y = (screen.height/2)-(h/2) - 55 + (Math.floor(Math.random() * 100)-50);
+win.moveTo(x,y);
+
+setTimeout(function () {
+intervalID  = window.setInterval(function () { bounce() }, interval);
+}, 2500);
+
+function bounce() {
+if (win.closed) {
+    clearInterval(intervalID);
+    return;
+}
+
+if ((x+dx > (screen.availWidth - w)) || (x+dx < 0)) dx = -dx;
+
+if ((y+dy > (screen.availHeight - h)) || (y+dy < 0)) {dy = -dy; x += 1};
+
+x += dx;
+y += dy;
+
+win.moveTo(x,y);
+win.focus();
+}
+
+}
+
+function xx() {
+var audio = new Audio('Coconut Mall - Mario Kart Wii OST.mp4');
+  setInterval(function () {
+   audio.play();
+    },1306)
+
+cow();
+}
+
+var ii = 0;
+window.onload = function () {
+document.getElementById('c').onclick = xx;
+setInterval(function () {
+  if (ii == 0) {
+    ii =1;
+    document.getElementById('c').style.color = 'white';
+  }
+  else if (ii == 1) {
+    ii = 0;
+    document.getElementById('c').style.color = 'black';
+  }
+
+}, 5);
+}
+
       y = (screen.height/2)-(h/2) - 55 + (Math.floor(Math.random() * 100)-50);
 var win = window.open('image.html', "",
       "width=" + w + ",height=" + h);
