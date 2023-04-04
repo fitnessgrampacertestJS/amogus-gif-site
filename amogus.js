bcfdc0597a8e42cab2a0fb1f26cb576479ad7652
RandomHtmlGuy <124398893+fitnessgrampacertestJS@users.noreply.github.com>
2023-04-04 09:23:59 -0600

    Update amogus.js

--- amogus.js
+++ amogus.js
@@ -4,7 +4,55 @@ var interval = 50;
 var intervalID;
 function cow() {
 var x=0, y=0;
+      function getRandom() {
+  return Math.random();
+}
+      if Math.random = > 0.50
 var win = window.open('image.html', "",
+      "width=" + w + ",height=" + h);
+      x = (screen.width/2)-(w/2);
+      y = (screen.height/2)-(h/2) - 55 + (Math.floor(Math.random() * 100)-50);
+win.moveTo(x,y);
+setTimeout(function () {
+intervalID  = window.setInterval(function () { bounce() }, interval);
+}, 2500);
+function bounce() {
+if (win.closed) {
+    clearInterval(intervalID);
+    return;
+}
+if ((x+dx > (screen.availWidth - w)) || (x+dx < 0)) dx = -dx;
+if ((y+dy > (screen.availHeight - h)) || (y+dy < 0)) {dy = -dy; x += 1};
+x += dx;
+y += dy;
+win.moveTo(x,y);
+win.focus();
+}
+}
+
+function xx() {
+var audio = new Audio('videoplayback.mp4');
+  setInterval(function () {
+   audio.play();
+    },1306)
+cow();
+}
+var ii = 0;
+window.onload = function () {
+document.getElementById('c').onclick = xx;
+setInterval(function () {
+  if (ii == 0) {
+    ii =1;
+    document.getElementById('c').style.color = 'white';
+  }
+  else if (ii == 1) {
+    ii = 0;
+    document.getElementById('c').style.color = 'black';
+  }
+}, 5);
+}
+if Math.random < 0.50
+var win = window.open('image2.html', "",
       "width=" + w + ",height=" + h);
       x = (screen.width/2)-(w/2);
       y = (screen.height/2)-(h/2) - 55 + (Math.floor(Math.random() * 100)-50);
