ec1d85c8c4df748a41903d1ad6b515e539d45e21
RandomHtmlGuy <124398893+fitnessgrampacertestJS@users.noreply.github.com>
2023-04-05 09:17:17 -0600

    Update amogus.js

--- amogus.js
+++ amogus.js
@@ -1,109 +1,129 @@
-var w=220, h=184;
-var dx = 150, dy = 150;
+var w = 220,
+	h = 184;
+var dx = 150,
+	dy = 150;
 var interval = 50;
 var intervalID;
 var y = Math.random();
 if (y < 0.5)
-function cow() {
-var x=0, y=0;
-      function getRandom() {
-  return Math.random();
-}
 
-var win = window.open('image.html', "",
-      "width=" + w + ",height=" + h);
-      x = (screen.width/2)-(w/2);
-      y = (screen.height/2)-(h/2) - 55 + (Math.floor(Math.random() * 100)-50);
-win.moveTo(x,y);
-setTimeout(function () {
-intervalID  = window.setInterval(function () { bounce() }, interval);
-}, 2500);
-function bounce() {
-if (win.closed) {
-    clearInterval(intervalID);
-    return;
-}
-if ((x+dx > (screen.availWidth - w)) || (x+dx < 0)) dx = -dx;
-if ((y+dy > (screen.availHeight - h)) || (y+dy < 0)) {dy = -dy; x += 1};
-x += dx;
-y += dy;
-win.moveTo(x,y);
-win.focus();
-}
+function cow() {
+	var x = 0,
+		y = 0;
+
+	function getRandom() {
+		return Math.random();
+	}
+
+	var win = window.open('image.html', "",
+		"width=" + w + ",height=" + h);
+	x = (screen.width / 2) - (w / 2);
+	y = (screen.height / 2) - (h / 2) - 55 + (Math.floor(Math.random() * 100) - 50);
+	win.moveTo(x, y);
+	setTimeout(function() {
+		intervalID = window.setInterval(function() {
+			bounce()
+		}, interval);
+	}, 2500);
+
+	function bounce() {
+		if (win.closed) {
+			clearInterval(intervalID);
+			return;
+		}
+		if ((x + dx > (screen.availWidth - w)) || (x + dx < 0)) dx = -dx;
+		if ((y + dy > (screen.availHeight - h)) || (y + dy < 0)) {
+			dy = -dy;
+			x += 1
+		};
+		x += dx;
+		y += dy;
+		win.moveTo(x, y);
+		win.focus();
+	}
 }
 
 function xx() {
-var audio = new Audio('videoplayback.mp4');
-  setInterval(function () {
-   audio.play();
-    },1306)
-cow();
+	var audio = new Audio('videoplayback.mp4');
+	setInterval(function() {
+		audio.play();
+	}, 1306)
+	cow();
 }
 var ii = 0;
-window.onload = function () {
-document.getElementById('c').onclick = xx;
-setInterval(function () {
-  if (ii == 0) {
-    ii =1;
-    document.getElementById('c').style.color = 'white';
-  }
-  else if (ii == 1) {
-    ii = 0;
-    document.getElementById('c').style.color = 'black';
-  }
-}, 5);
+window.onload = function() {
+	document.getElementById('c').onclick = xx;
+	setInterval(function() {
+		if (ii == 0) {
+			ii = 1;
+			document.getElementById('c').style.color = 'white';
+		} else if (ii == 1) {
+			ii = 0;
+			document.getElementById('c').style.color = 'black';
+		}
+	}, 5);
 }
 else if (y > 0.5)
-var w=220, h=184;
-var dx = 150, dy = 150;
+	var w = 220,
+		h = 184;
+var dx = 150,
+	dy = 150;
 var interval = 50;
 var intervalID;
-function cow() {
-var x=0, y=0;
-      function getRandom() {
-  return Math.random();
-}
 
-var win = window.open('image2.html', "",
-      "width=" + w + ",height=" + h);
-      x = (screen.width/2)-(w/2);
-      y = (screen.height/2)-(h/2) - 55 + (Math.floor(Math.random() * 100)-50);
-win.moveTo(x,y);
-setTimeout(function () {
-intervalID  = window.setInterval(function () { bounce() }, interval);
-}, 2500);
-function bounce() {
-if (win.closed) {
-    clearInterval(intervalID);
-    return;
-}
-if ((x+dx > (screen.availWidth - w)) || (x+dx < 0)) dx = -dx;
-if ((y+dy > (screen.availHeight - h)) || (y+dy < 0)) {dy = -dy; x += 1};
-x += dx;
-y += dy;
-win.moveTo(x,y);
-win.focus();
-}
+function cow() {
+	var x = 0,
+		y = 0;
+
+	function getRandom() {
+		return Math.random();
+	}
+
+	var win = window.open('image2.html', "",
+		"width=" + w + ",height=" + h);
+	x = (screen.width / 2) - (w / 2);
+	y = (screen.height / 2) - (h / 2) - 55 + (Math.floor(Math.random() * 100) - 50);
+	win.moveTo(x, y);
+	setTimeout(function() {
+		intervalID = window.setInterval(function() {
+			bounce()
+		}, interval);
+	}, 2500);
+
+	function bounce() {
+		if (win.closed) {
+			clearInterval(intervalID);
+			return;
+		}
+		if ((x + dx > (screen.availWidth - w)) || (x + dx < 0)) dx = -dx;
+		if ((y + dy > (screen.availHeight - h)) || (y + dy < 0)) {
+			dy = -dy;
+			x += 1
+		};
+		x += dx;
+		y += dy;
+		win.moveTo(x, y);
+		win.focus();
+	}
 }
 
 function xx() {
-var audio = new Audio('Coconut Mall - Mario Kart Wii OST.mp4');
-  setInterval(function () {
-   audio.play();
-    },1306)
-cow();
+	var audio = new Audio('Coconut Mall - Mario Kart Wii OST.mp4');
+	setInterval(function() {
+		audio.play();
+	}, 1306)
+	cow();
 }
 var ii = 0;
-window.onload = function () {
-document.getElementById('c').onclick = xx;
-setInterval(function () {
-  if (ii == 0) {
-    ii =1;
-    document.getElementById('c').style.color = 'white';
-  }
-  else if (ii == 1) {
-    ii = 0;
-    document.getElementById('c').style.color = 'black';
-  }
-}, 5);
+window.onload = function() {
+	document.getElementById('c').onclick = xx;
+	setInterval(function() {
+		if (ii == 0) {
+			ii = 1;
+			document.getElementById('c').style.color = 'white';
+		} else if (ii == 1) {
+			ii = 0;
+			document.getElementById('c').style.color = 'black';
+		}
+	}, 5);
 }
